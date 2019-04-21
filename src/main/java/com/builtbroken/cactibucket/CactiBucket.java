784dc3325a2230d0fcab697e69f5e468ac1573b2
Bernie-G <berniegandin@gmail.com>
2019-04-20 22:52:33 -0400

    Updated to 1.10.2

--- src/main/java/com/builtbroken/cactibucket/CactiBucket.java
+++ src/main/java/com/builtbroken/cactibucket/CactiBucket.java
@@ -1,34 +1,53 @@
 package com.builtbroken.cactibucket;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 1/5/2017.
  */
 @Mod(modid = "cactibucket", name = "Cacti Bucket", version = CactiBucket.VERSION)
+@Mod.EventBusSubscriber()
 public class CactiBucket
 {
-    public static final String MAJOR_VERSION = "@MAJOR@";
-    public static final String MINOR_VERSION = "@MINOR@";
-    public static final String REVISION_VERSION = "@REVIS@";
-    public static final String BUILD_VERSION = "@BUILD@";
+    public static final String MAJOR_VERSION = "1";
+    public static final String MINOR_VERSION = "1";
+    public static final String REVISION_VERSION = "0";
+    public static final String BUILD_VERSION = "1";
     public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION + "." + BUILD_VERSION;
 
-    public static ItemCactiBucket itemBucket;
+    public static ItemCactiBucket itemBucket = new ItemCactiBucket();
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().registerAll(itemBucket);
+    }
+    @SubscribeEvent
+    public static void registerRenders(ModelRegistryEvent event)
+    {
+        ModelLoader.setCustomModelResourceLocation(itemBucket, 0, new ModelResourceLocation(itemBucket.getRegistryName(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(itemBucket, 1, new ModelResourceLocation(itemBucket.getRegistryName() + ".water", "inventory"));
+    }
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        itemBucket = new ItemCactiBucket();
-        GameRegistry.registerItem(itemBucket, "cbCactiBucket");
+
     }
 
     @Mod.EventHandler
@@ -41,6 +60,7 @@ public class CactiBucket
     public void postInit(FMLPostInitializationEvent event)
     {
         itemBucket.postInit();
-        GameRegistry.addShapedRecipe(new ItemStack(itemBucket), "SSS", "L L", "LLL", 'L', Blocks.cactus, 'S', Items.stick);
+        GameRegistry.addShapedRecipe(new ItemStack(itemBucket), "SSS", "L L", "LLL", 'L', Blocks.CACTUS, 'S', Items.STICK);
     }
+
 }
